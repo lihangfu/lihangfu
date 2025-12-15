18701173c281f3d7f21843f30ddc0e2168917247
lihangfu <hfli8@iflytek.com>
2025-12-15 09:44:33 +0800

    feat: 增加tailwindcss 以及 motion 动画支持

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,10 +7,12 @@ import svgLoader from 'vite-svg-loader'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
     svgLoader(),
