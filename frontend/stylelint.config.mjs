91b395bbce9d9535463573c55adcf2f798041efa
lihangfu <hfli8@iflytek.com>
2025-12-15 17:28:46 +0800

    feat: 引入 stylelint

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -6,7 +6,10 @@ export default {
     'stylelint-config-html/vue',
     'stylelint-config-recess-order',
   ],
+  plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-prettier'],
+  // 忽略的文件
   ignoreFiles: ['dist/**/*', 'node_modules/**/*', 'public/**/*', 'src/style/reset.scss'],
+  // 按文件类型 / 路径，覆盖部分配置（和合并全局配置，但是优先级更高）
   overrides: [
     {
       files: ['**/*.(css|html|vue)'],
