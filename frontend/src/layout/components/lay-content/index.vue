e341eeeb2ed356271b3aff9896c2b760300ceea0
lihangfu <hfli8@iflytek.com>
2025-12-16 10:41:54 +0800

    feat: 主体布局

---
+++ frontend/src/layout/components/lay-content/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <router-view />
+  </section>
+</template>
--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
+import { defineComponent, h } from 'vue'
 import NavVertical from './components/lay-sidebar/NavVertical.vue'
+import LayContent from './components/lay-content/index.vue'
+
+const LayHeader = defineComponent({
+  name: 'LayHeader',
+  render() {
+    return h('div')
+  },
+})
 </script>
 
 <template>
   <div ref="appWrapperRef" :class="['app-wrapper']">
+    <!-- 侧边栏 -->
     <NavVertical />
-    <div>
-      <div>主体头部</div>
-      <div>主体内容</div>
-      <RouterView />
+    <!-- 主区域 -->
+    <div :class="['main-container']">
+      <div>
+        <LayHeader />
+        <LayContent />
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+}
+</style>
